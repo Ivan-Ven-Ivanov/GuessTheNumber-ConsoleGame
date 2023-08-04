92205dfa0b288f7e08a467074462a77c702bb76b
IvanVIvanov <ivveni@abv.bg>
2023-08-04 10:29:42 +0300

    Added the project

---
+++ GuessTheNumber.cs
@@ -0,0 +1,37 @@
+﻿namespace GuessTheNumber
+{
+    internal class GuessTheNumber
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+            while (true)
+            {
+                Console.Write("Guess the number (1-100): ");
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+                if (isValid)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if(playerNumber > computerNumber)
+                    {
+                        Console.WriteLine("Too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+        }
+    }
+}
