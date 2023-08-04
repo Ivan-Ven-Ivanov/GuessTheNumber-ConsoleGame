cd1a343bcfc85cb2bc0668ea802f6aa50136bcfa
IvanVIvanov <ivveni@abv.bg>
2023-08-04 11:37:30 +0300

    Added difficulty choice

--- GuessTheNumber.cs
+++ GuessTheNumber.cs
@@ -6,8 +6,29 @@
         {
             while (true)
             {
+                Console.Write("Choose difficulty: [e]asy, [m]edium or [h]ard: ");
+                string difficulty = Console.ReadLine().ToLower();
+                if (difficulty == "e" || difficulty == "easy")
+                {
+                    Console.WriteLine("Easy difficulty - you have infinite guesses!");
+                }
+                else if (difficulty == "m" || difficulty == "medium")
+                {
+                    Console.WriteLine("Medium difficulty - you have 10 guesses!");
+                }
+                else if (difficulty == "h" || difficulty == "hard")
+                {
+                    Console.WriteLine("Hard difficulty - you have 5 guesses!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+
                 Random randomNumber = new Random();
                 int computerNumber = randomNumber.Next(1, 101);
+                int guessesCount = 0;
                 while (true)
                 {
                     Console.Write("Guess the number (1-100): ");
@@ -23,10 +44,24 @@
                         else if (playerNumber > computerNumber)
                         {
                             Console.WriteLine("Too high");
+                            guessesCount++;
                         }
                         else
                         {
                             Console.WriteLine("Too low");
+                            guessesCount++;
+                        }
+
+                        if ((difficulty == "m" || difficulty == "medium") && guessesCount >= 10)
+                        {
+                            Console.WriteLine("Out of guesses. You lose.");
+                            break;
+                        }
+
+                        if ((difficulty == "h" || difficulty == "hard") && guessesCount >= 5)
+                        {
+                            Console.WriteLine("Out of guesses. You lose.");
+                            break;
                         }
                     }
                     else
@@ -45,7 +80,7 @@
                     }
                     else if (playAgain == "no")
                     {
-                       return;
+                        return;
                     }
                     else
                     {
